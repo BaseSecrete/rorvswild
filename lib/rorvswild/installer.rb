f7e000aeb71cd2545f003e4f3051332b269981ed
Alexis Bernard <alexis@bernard.io>
2017-02-07 16:17:13 +0100

    Install script.

---
+++ lib/rorvswild/installer.rb
@@ -0,0 +1,33 @@
+module RorVsWild
+  class Installer
+    PATH = "config/rorvswild.yml"
+
+    def self.create_rails_config(api_key)
+      if File.directory?("config")
+        if !File.exists?(PATH)
+          File.write(PATH, template(api_key))
+          puts "File #{PATH} has been created. Restart / deploy your app to start collecting data."
+        else
+          puts "File #{PATH} already exists."
+        end
+      else
+        puts "There is no config directory to create #{PATH}."
+      end
+    end
+
+    def self.template(api_key)
+      <<YAML
+# Keep the development block for testing on your local machine only.
+development:
+  api_key: #{api_key}
+
+production:
+  api_key: #{api_key}
+  # explain_sql_threshold: 500  # Execute EXPLAIN for queries above the specified time in ms.
+  # ignored_exceptions:
+  #   - ActionController::RoutingError
+  #   - UncommentToIgnoreAnyExceptionNameListedHere
+YAML
+    end
+  end
+end
