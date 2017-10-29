3b7760f48b9a505f7c451b06e17273c35a6a483f
Alexis Bernard <alexis@bernard.io>
2017-10-29 12:26:25 +0100

    NetHTTP plugin does not profile when an elasticsearch query is tracked.

--- test/plugin/elasticsearch_test.rb
+++ test/plugin/elasticsearch_test.rb
@@ -9,9 +9,9 @@ class RorVsWild::Plugin::ElasticsearchTest < Minitest::Test
     agent.measure_block("elastic") do
       ::Elasticsearch::Client.new.search(q: "test")
     end
-    assert_equal(2, agent.data[:sections].size) # TODO: Ignore HTTP
-    assert_equal(1, agent.data[:sections][1].calls)
-    assert_equal("elasticsearch", agent.data[:sections][1].kind)
-    assert_equal('{"method":"GET","path":"_search","params":{"q":"test"},"body":null}', agent.data[:sections][1].command)
+    assert_equal(1, agent.data[:sections].size)
+    assert_equal(1, agent.data[:sections][0].calls)
+    assert_equal("elasticsearch", agent.data[:sections][0].kind)
+    assert_equal('{"method":"GET","path":"_search","params":{"q":"test"},"body":null}', agent.data[:sections][0].command)
   end
 end
