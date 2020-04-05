26eaed67af941db7fed6049fb9b835f5b1bf3b43
Alexis Bernard <alexis@basesecrete.com>
2020-04-05 17:34:40 +0200

    Fix ensure block for old Ruby versions

--- lib/rorvswild/plugin/active_job.rb
+++ lib/rorvswild/plugin/active_job.rb
@@ -10,13 +10,15 @@ module RorVsWild
 
       def self.around_perform(job, block)
         RorVsWild.agent.measure_job(job.class.name, parameters: job.arguments) do
-          section = RorVsWild::Section.start
-          section.command = "#{job.class}#perform"
-          section.file, section.line = job.method(:perform).source_location
-          section.file = RorVsWild.agent.locator.relative_path(section.file)
-          block.call
-        ensure
-          RorVsWild::Section.stop
+          begin
+            section = RorVsWild::Section.start
+            section.command = "#{job.class}#perform"
+            section.file, section.line = job.method(:perform).source_location
+            section.file = RorVsWild.agent.locator.relative_path(section.file)
+            block.call
+          ensure
+            RorVsWild::Section.stop
+          end
         end
       end
     end
