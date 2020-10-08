7f0df9949cc35118008bf58bbb2a3512ea425631
Alexis Bernard <alexis@basesecrete.com>
2020-10-08 06:59:45 +0200

    Ensure Resque is not installed twice

--- lib/rorvswild/plugin/resque.rb
+++ lib/rorvswild/plugin/resque.rb
@@ -2,7 +2,9 @@ module RorVsWild
   module Plugin
     module Resque
       def self.setup
+        return if @installed
         ::Resque::Job.send(:extend, Resque) if defined?(::Resque::Job)
+        @installed = true
       end
 
       def around_perform_rorvswild(*args, &block)
