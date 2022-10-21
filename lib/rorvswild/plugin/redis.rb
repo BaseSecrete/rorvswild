463218e40dde8e22bfd1eda8a2e3cc532800c375
Alexis Bernard <alexis@basesecrete.com>
2022-10-21 15:38:28 +0200

    Fix plugin for Redis 5

--- lib/rorvswild/plugin/redis.rb
+++ lib/rorvswild/plugin/redis.rb
@@ -3,18 +3,32 @@ module RorVsWild
     class Redis
       def self.setup
         return if !defined?(::Redis)
-        return if ::Redis::Client.method_defined?(:process_without_rorvswild)
-        ::Redis::Client.class_eval do
-          alias_method :process_without_rorvswild, :process
+        if ::Redis::Client.method_defined?(:process)
+          ::Redis::Client.prepend(V4)
+        else
+          ::Redis::Client.prepend(V5)
+        end
+      end
 
-          def process(commands, &block)
-            string = RorVsWild::Plugin::Redis.commands_to_string(commands)
-            appendable = RorVsWild::Plugin::Redis.appendable_commands?(commands)
-            RorVsWild.agent.measure_section(string, appendable_command: appendable, kind: "redis".freeze) do
-              process_without_rorvswild(commands, &block)
-            end
+      module V4
+        def process(commands, &block)
+          string = RorVsWild::Plugin::Redis.commands_to_string(commands)
+          appendable = RorVsWild::Plugin::Redis.appendable_commands?(commands)
+          RorVsWild.agent.measure_section(string, appendable_command: appendable, kind: "redis".freeze) do
+            super(commands, &block)
           end
         end
+      end
+
+      module V5
+        def call_v(commands, &block)
+          appendable = RorVsWild::Plugin::Redis.appendable_commands?(commands)
+          RorVsWild.agent.measure_section(commands[0], appendable_command: appendable, kind: "redis".freeze) do
+            super(commands, &block)
+          end
+        end
+
+        # TODO: Monitor pipelined
       end
 
       def self.commands_to_string(commands)
--- test/plugin/redis_test.rb
+++ test/plugin/redis_test.rb
@@ -6,18 +6,17 @@ class RorVsWild::Plugin::RedisTest < Minitest::Test
   include RorVsWild::AgentHelper
 
   def test_callback
-    url = "redis://localhost:6379/1"
-    agent.measure_code("::Redis.new(url: '#{url}').get('foo')")
+    agent.measure_code("::Redis.new.get('foo')")
     assert_equal(1, agent.current_data[:sections].size)
     assert_equal("redis", agent.current_data[:sections][0].kind)
-    assert_equal("select\nget", agent.current_data[:sections][0].command)
+    assert_equal("get", agent.current_data[:sections][0].command.to_s)
   end
 
   def test_callback_when_pipelined
     agent.measure_block("pipeline") do
-      (redis = ::Redis.new).pipelined do
-        redis.get("foo")
-        redis.set("foo", "bar")
+      (redis = ::Redis.new).pipelined do |pipeline|
+        pipeline.get("foo")
+        pipeline.set("foo", "bar")
       end
     end
     assert_equal(1, agent.current_data[:sections].size)
