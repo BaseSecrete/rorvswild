6ce1dfd67b0f28b0c85544eaa95c83f30c57a700
Alexis Bernard <alexis@basesecrete.com>
2025-06-26 18:25:20 +0200

    Normalize MongoDB queries

--- test/plugin/mongo_test.rb
+++ test/plugin/mongo_test.rb
@@ -26,14 +26,49 @@ class RorVsWild::Plugin::MongoTest < Minitest::Test
 
     assert_equal(1, sections[0].calls)
     assert_equal("mongo", sections[0].kind)
-    assert_match({drop: "mountains"}.to_json, sections[0].command)
+    assert_equal({drop: "mountains"}.to_json, sections[0].command)
 
     assert_equal(2, sections[1].calls)
     assert_equal("mongo", sections[1].kind)
-    assert_match({insert: "mountains"}.to_json, sections[1].command)
+    assert_equal({insert: "mountains"}.to_json, sections[1].command)
 
     assert_equal(1, sections[2].calls)
     assert_equal("mongo", sections[2].kind)
-    assert_match({find: "mountains"}.to_json, sections[2].command)
+    assert_equal({find: "mountains", filter: {altitude: {"$gt": "?"} }}.to_json, sections[2].command)
+  end
+
+  def test_normalize_query
+    plugin = RorVsWild::Plugin::Mongo.new
+
+    query = {"find" => "collection", "filter" => {"some_id" => 123, "date": {"$gt": "2015-06-06"}}, "$db" => "db", "lsid"=>{"id"=>123}}
+    assert_equal({"find" => "collection", "filter" => {"some_id" => "?", "date": {"$gt": "?"}}}, plugin.normalize_query(query))
+
+    query = {"getMore" => 123, "collection" => "collection", "$db": "db", "lsid": {id: 123}}
+    assert_equal({"getMore" => "?", "collection" => "collection"}, plugin.normalize_query(query))
+
+    query = {
+      "find" => "collection",
+      "filter" => {
+        "id" => 123,
+        "bool" => {"$ne" => true},
+        "$or" => [{"date" => {"$gt" => "2015-06-06"}}, {"date" => nil}]
+      },
+      "projection" => {col1: 0, col2: 0},
+      "sort" => {"date" => 1},
+      "$db" => "db",
+      "lsid" => {"id" => 123}
+    }
+    assert_equal(
+      {
+        "find" => "collection",
+        "filter" => {
+          "id" => "?",
+          "bool" => {"$ne" => true},
+          "$or" => [{"date" => {"$gt" => "?"}}, {"date" => nil}]
+        },
+        "sort" => {"date" => 1},
+      },
+      plugin.normalize_query(query)
+    )
   end
 end
